ca0ef6443dafb47136febca9c4aca8be7ada18a7
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-04-23 19:13:43 -0400

    Influencer getAll, and create(almost done)

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Route, Switch } from 'react-router-dom'
-import ProductList from './components/products/ProductList'
+// import ProductList from './components/products/ProductList'
+import InfluencerList from './components/influencer/InfluencerList'
 
 export default () => (
 <Switch>
+  <Route exact path="/" component={InfluencerList} />
   {/* <Route exact path="/" component={Index} /> */}
-  <Route path="/products" component={ProductList} />
+  {/* <Route path="/products" component={ProductList} /> */}
+
 </Switch>
 
 );
