eb9dd08ceffcf77ae6ec7b3298b72abccb6a92c6
Antonio Colmenares <colmena.anto@gmail.com>
2019-05-09 19:01:26 +0200

    created middleware for admin that is working and already secured the /reviewForm page

--- src/components/reviews/ReviewForm.js
+++ src/components/reviews/ReviewForm.js
@@ -7,7 +7,7 @@ import { Redirect} from 'react-router-dom'
 import { Input } from 'antd';
 const { TextArea } = Input;
 
-class InfluencerReviewForm extends Component{
+class ReviewForm extends Component{
     state= {
       
         title:"",
@@ -66,8 +66,8 @@ class InfluencerReviewForm extends Component{
           review: this.state.review,
           influencer: this.state.influencer,
           video: this.state.video,
-          voicenote: this.state.voicenote
-
+          voicenote: this.state.voicenote,
+          user: this.props.user
         })
         .then( res => {
           this.setState({created:true,reviewDone:res})
@@ -80,33 +80,36 @@ class InfluencerReviewForm extends Component{
     
 
     render(){
-
-      if(this.state.created){
-        return(<Redirect to="/"></Redirect>)
-      }else{
-        return(
-          <div>  
-            {/* <input type="file" onChange={this.handleChange}></input>
-            <button onClick={this.handleSubmit}>Add Voicento</button>  
-            <AudioPlayer
-              autoPlay
-              src="https://res.cloudinary.com/dcopnvq47/video/upload/v1556903515/BEBE_-_6ix9ine_Ft._Anuel_AA_Prod._By_Ronny_J_Official_Music_Video_ccohlm.mp3"
-              onPlay={e => console.log("onPlay")}
-              // other props here
-            /> */}
-            <Input name="title" placeholder="Please enter title " allowClear onChange={this.onChange} />
-            <TextArea rows={4} name="review" placeholder="Please enter review of review" onChange={this.onChange} />
-            <Input name="influencer" placeholder="Please enter infleuncer ID " allowClear onChange={this.onChange} />
-            <Input name="video" placeholder="Please enter VIDEO URL CLOUDINARY " allowClear onChange={this.onChange} />
-            <Input name="voicenote" placeholder="Please enter VOICENOTE URL CLOUDINARY " allowClear onChange={this.onChange} />
-
-
-            {/* <Input name="voicenote" placeholder="Please enter product description" allowClear onChange={this.onChange} /> */}
-           <button onClick={this.handleSubmit}>Submit</button>
-          </div>
-        )
-          }   
+      if (this.props.user.role === "Admin"){
+        if(this.state.created){
+          return(<Redirect to="/"></Redirect>)
+        }else{
+          return(
+            <div>  
+              {/* <input type="file" onChange={this.handleChange}></input>
+              <button onClick={this.handleSubmit}>Add Voicento</button>  
+              <AudioPlayer
+                autoPlay
+                src="https://res.cloudinary.com/dcopnvq47/video/upload/v1556903515/BEBE_-_6ix9ine_Ft._Anuel_AA_Prod._By_Ronny_J_Official_Music_Video_ccohlm.mp3"
+                onPlay={e => console.log("onPlay")}
+                // other props here
+              /> */}
+              <Input name="title" placeholder="Please enter title " allowClear onChange={this.onChange} />
+              <TextArea rows={4} name="review" placeholder="Please enter review of review" onChange={this.onChange} />
+              <Input name="influencer" placeholder="Please enter infleuncer ID " allowClear onChange={this.onChange} />
+              <Input name="video" placeholder="Please enter VIDEO URL CLOUDINARY " allowClear onChange={this.onChange} />
+              <Input name="voicenote" placeholder="Please enter VOICENOTE URL CLOUDINARY " allowClear onChange={this.onChange} />
+  
+  
+              {/* <Input name="voicenote" placeholder="Please enter product description" allowClear onChange={this.onChange} /> */}
+             <button onClick={this.handleSubmit}>Submit</button>
+            </div>
+          )
+            }   
+  } else {
+    return( <Redirect to="/"></Redirect>)
+  }
 }
 }
 
-export default InfluencerReviewForm;
+export default ReviewForm;
