35c1823af1b3f80ea8103bd21e785cc9e95d4826
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-16 12:54:24 -0400

    Orders with coookies

--- src/services/OrderServices.js
+++ src/services/OrderServices.js
@@ -4,7 +4,13 @@ const getCart = (userId) => http.get(`order/${userId}`)
   .then(response => response.data);
 
 const payCart = (user,cardname,address,addressCity,addressZip) => http.post(`order/payment`,user,cardname,address,addressCity,addressZip)
+  .then(response=>response.data)
+
+  const createOrder = (user,cardname,address,addressCity,addressZip) => http.post(`order/add`,user,cardname,address,addressCity,addressZip)
+  .then(response=>response.data)
+
 export default {
   getCart,
-  payCart
+  payCart,
+  createOrder
 }
