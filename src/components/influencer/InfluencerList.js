8760bcf4c5e4336c92d3d0b2ba0f5ced762a7e7e
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-20 13:44:52 -0400

    Added Search bar for influencers

--- src/components/influencer/InfluencerList.js
+++ src/components/influencer/InfluencerList.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+
+import axios from 'axios'
+import SearchBar from './SearchInfluencer'
 import InfluencerServices from '../../services/InfluencerServices'
 import AuthServices from '../../services/AuthServices'
+
 import {Link} from 'react-router-dom'
 import Slider from "react-slick";
 
@@ -26,6 +30,15 @@ class InfluencerList extends Component{
           }))
       }
 
+      getFilter = (e) => { 
+
+        let url = "http://localhost:3002/influencer/filter?search=" + e.target.value
+        axios.get(url,{withCredentials:true})
+        .then((res)=>{
+            this.setState({influencers:res.data})
+        })
+        .catch((err)=>console.log(err))
+    }
 
     componentDidMount(){
         this.fetchInfluencer()
@@ -33,15 +46,16 @@ class InfluencerList extends Component{
     }
 
     render(){
-        const {influencers} = this.state
+        const {influencers,productsFiltered} = this.state
         return(
             <div>
                 <h1>Influencer landing</h1>
+                <div>
+                <SearchBar products={productsFiltered} getFilter={this.getFilter} />
+                </div>
                 <Slider {...settings}>
-
                 {influencers.map((i,index)=>{
                     return(
-
                         <Link key={i}to={`review/${i._id}`}>
                         <div key={index} className="influencer-card">
                         <p>name: {i.name}</p>
