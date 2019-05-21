8c1eb15ab2ca779fc60d3615f3ec8f7b5204540a
Antonio Colmenares <colmena.anto@gmail.com>
2019-05-21 17:27:49 +0200

    added bootstrap quantity and its adding to the state

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -32,68 +32,92 @@ class CardItem extends Component {
   }
 
 
-  onChange(value) {
-    if (value > this.state.value){
-      console.log('value', value);
-      console.log("state", this.state.value);
-      var newAmount = value - this.state.value;
-      console.log("new amount", newAmount);
+  onChange = (e) => {
+
+    this.setState({
+      value: e.target.value
+    })
+
+    console.log(e.target.value);
+    // if (value !== "" && value <= 9 || value !== null && value <= 9 ){
+    //   this.setState({
+    //     value: value
+    //   })
+    // } else if (value > 9){
+    //   console.log("max value is 9 for bigger purchases contact us in the about me section")
+    // }
+    // console.log("this is the value in the state",this.state.value);
+    // if (value > this.state.value){
+    //   console.log('value', value);
+    //   console.log("state", this.state.value);
+    //   var newAmount = value - this.state.value;
+    //   console.log("new amount", newAmount);
   
-      if(value !== ""){
-        this.setState({
-          value: value
-        })
-      }
-      console.log("this is the new amount", newAmount);
+    //   if(value !== ""){
+    //     this.setState({
+    //       value: value
+    //     })
+    //   }
+    //   console.log("this is the new amount", newAmount);
       
   
-      for (var i = this.state.value; i < value; i++){
-        axios.post("http://localhost:3002/order" , {
-          user: this.props.user._id,
-          _id: this.props.product._id
-        }, {withCredentials:true})
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err))
-      }
-    } else {
+    //   for (var i = this.state.value; i < value; i++){
+    //     axios.post("http://localhost:3002/order" , {
+    //       user: this.props.user._id,
+    //       _id: this.props.product._id
+    //     }, {withCredentials:true})
+    //       .then((res) => console.log(res))
+    //       .catch((err) => console.log(err))
+    //   }
+    // } else {
 
-      if (value !== ""){
-        const url = `http://localhost:3002/order/delete/${this.props.product._id}`;
+    //   if (value !== ""){
+    //     const url = `http://localhost:3002/order/delete/${this.props.product._id}`;
 
-        axios.post(url , {
-          user: this.props.user
-        }, {withCredentials:true})
-          .then((res) => {
-            for (var i = 0; i < value; i++){
-              axios.post("http://localhost:3002/order" , {
-                user: this.props.user._id,
-                _id: this.props.product._id
-              }, {withCredentials:true})
-                .then((res) => console.log(res))
-                .catch((err) => console.log(err))
-            }
-          })
-          .catch((err) => err)
-
-          this.setState({
-            value: value
-          })
+    //     axios.post(url , {
+    //       user: this.props.user
+    //     }, {withCredentials:true})
+    //       .then((res) => {
+    //         for (var i = 0; i < value; i++){
+    //           axios.post("http://localhost:3002/order" , {
+    //             user: this.props.user._id,
+    //             _id: this.props.product._id
+    //           }, {withCredentials:true})
+    //             .then((res) => console.log(res))
+    //             .catch((err) => console.log(err))
+    //         }
+    //       })
+    //       .catch((err) => err)
 
 
-      }
-    }
+    //   }
+    // }
   }
 
   render(){
-    console.log(this.state.product)
+    // console.log(this.state.product)
 
   
     return(
       <ul>
         <li>{this.props.product.model}</li>
         <li>Price:{this.props.product.prize}</li>
-        <li>Quantity:<InputNumber min={0} max={9} defaultValue={this.props.times} onChange={this.onChange.bind(this)} /></li>
-        
+        {/* <li>Quantity:<InputNumber min={0} max={9} defaultValue={this.props.times} onChange={this.onChange.bind(this)} /></li> */}
+        <li>
+          <div className="input-group mb-3">
+            <select className="custom-select" id="inputGroupSelect01" defaultValue={this.props.times} onChange={(e) => this.onChange(e)}>
+              {/* <option selected>Choose...</option> */}
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+            </select>
+          </div>
+        </li>
       </ul>
     )
   }
