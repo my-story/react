12a23ef38b761d711ff1453feb5fc40d2fb5ec94
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-23 15:28:59 -0400

    use validatoin address

--- src/components/cart/ShippingItems.js
+++ src/components/cart/ShippingItems.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Input } from 'antd';
+import * as toastr from 'toastr'
 
 import Checkout from '../Payment/Checkout'
+import OrderServices from '../../services/OrderServices'
 import Cookies from 'universal-cookie';
+
 
 
 class ShippingRates extends Component{
     state={
         products:"",
-        address:{}
+        address:{
+            name:"",
+            company:"",
+            street1:"",
+            city:"",
+            state:"",
+            zip:"",
+            country:""
+        }
     }
 
     componentDidMount(){
@@ -20,27 +32,48 @@ class ShippingRates extends Component{
         var counter = 0;
         const cookies = new Cookies();
         const cookieArr = cookies.get("Products");
-  
-        for (var i = 0; i < cookieArr.length; i++){
-          counter += (cookieArr[i].prize * cookieArr[i].qty)
+        if(cookieArr !== undefined){
+            for (var i = 0; i < cookieArr.length; i++){
+                counter += (cookieArr[i].prize * cookieArr[i].qty)
+              }
         }
 
         return counter;
   }
 
-    onChange = e => {
-        let { address } = this.state
-        address[e.target.name] = e.target.value
-        this.setState({ address })
+  onChange = e => {
+    let { address } = this.state
+    address[e.target.name] = e.target.value
+    this.setState({ address })
+}
+
+    validateAddress = ()=> {
+        const { address } = this.state
+
+        // if( address.name===undefined || address.street1=== undefined || address.city=== undefined || address.state=== undefined || address.zip=== undefined || address.country=== undefined){
+        // toastr.error("Missing Fields")
+        // console.log("No te deja comprar ")
+        // return
+        // }else{
+            OrderServices.checkAddress(address)
+            .then((res)=>{
+                console.log(res)
+                // this.setState({address:res.data})
+            })
+            .catch((e)=>console.log(e))
+        // }
     }
+
 
     render(){
         
-        console.log(this.state)
-      
+        console.log(this.state.address)
+        if(this.getTotal() === 0){
+            return(<Redirect to="/"></Redirect>)
+        }
         return(
-            <div>
 
+            <div>
                 <Input name="name" placeholder="Enter Name" onChange={this.onChange} />
                 <Input name="company" placeholder="Enter Company" onChange={this.onChange} />
                 <Input name="street1" placeholder="Enter Complete Street" onChange={this.onChange} />
@@ -49,8 +82,10 @@ class ShippingRates extends Component{
                 <Input name="zip" type="number" placeholder="Enter Zipcode" onChange={this.onChange} />
                 <Input name="country" placeholder="Enter Country" onChange={this.onChange} />
                 <div>
-
+                
+                <button onClick={()=>this.validateAddress()}>Check address</button>
                 <Checkout 
+                // onClick={this.validateAddress()}
                 name={`You have ${this.state.products.length}# of item(s)`}
                 description={"thank you for buying with my story"}
                 amount={this.getTotal()}
--- src/services/OrderServices.js
+++ src/services/OrderServices.js
@@ -6,11 +6,15 @@ const getCart = (userId) => http.get(`order/${userId}`)
 const payCart = (user,cardname,address,addressCity,addressZip) => http.post(`order/payment`,user,cardname,address,addressCity,addressZip)
   .then(response=>response.data)
 
-  const createOrder = (user, products ,email, cardname,address,addressCity,addressZip) => http.post(`order/add`,user,products,email, cardname,address,addressCity,addressZip)
-  .then(response=>response.data)
+const createOrder = (user, products ,email, cardname,address,addressCity,addressZip) => http.post(`order/add`,user,products,email, cardname,address,addressCity,addressZip)
+.then(response=>response.data)
+
+const checkAddress = (address, name,company,street1,city,state,zip,country) => http.post('shipping/validate',address,name,company,street1,city,state,zip,country)
+  .then(response => response.data)
 
 export default {
   getCart,
   payCart,
-  createOrder
+  createOrder,
+  checkAddress
 }
