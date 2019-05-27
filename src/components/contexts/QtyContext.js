27702d171d38415cb91f34ac850df1e6192072f0
Antonio Colmenares <colmena.anto@gmail.com>
2019-05-27 20:02:57 +0200

    created contexts

---
+++ src/components/contexts/QtyContext.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QtyContext = React.createContext();
+
+const QtyProvider = QtyContext.Provider;
+const QtyConsumer = QtyContext.Consumer; 
+
+
+export {QtyConsumer, QtyProvider};
