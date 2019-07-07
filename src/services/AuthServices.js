f6790c56fbc83f6a4d42a0235c14f1a29a42436c
Antonio Colmenares <colmena.anto@gmail.com>
2019-07-07 02:55:29 +0200

    still needs fixing

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -4,7 +4,7 @@ const signup = (user)  => http.post('api', user)
   .then(response => response);
 
 const login = (user) => http.post('api/login', user)
-  .then(response => response)
+  .then(response => response);
 
 const logout = (user) => http.post('api/logout', user)
   .then(response => response);
--- src/services/InfluencerServices.js
+++ src/services/InfluencerServices.js
@@ -1,14 +1,21 @@
 import http from './BaseServices'
 
-const createInfluencer = (data) =>
-  http.post('/influencer/create', data)
-    .then((res)=> res.data);
+const createInfluencer = (data) => http.post('/influencer/create', data)
+  .then((res)=> res.data);
 
-const getAll = () => 
-  http.get('/influencer/all')
-    .then((res) => res.data);
+const getAll = () => http.get('/influencer/all')
+  .then((res) => res.data);
 
-    export default {
-        createInfluencer,
-        getAll
-      }
+const deleteInfluencer = (user, id) => http.post(`/influencer/delete/${id}`, user)
+  .then((res) => res);
+
+const getFilter = (search) => http.get(`/influencer/filter?search=${search}`)
+  .then((res) => res.data);
+
+
+export default {
+    createInfluencer,
+    getAll,
+    deleteInfluencer,
+    getFilter
+}
