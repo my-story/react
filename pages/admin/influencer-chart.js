c23feb0e8b74c86370f6b3a40612b19241fb9a68
Antonio Colmenares <colmena.anto@gmail.com>
2019-09-12 22:56:17 +0200

    created admin panel controllers

---
+++ pages/admin/influencer-chart.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import AdminPanel from "../../src/components/admin/AdminPanel";
+
+class InfluencerChart extends Component {
+  state = { 
+
+  }
+
+
+  render() {
+    return(
+      <div>
+        <AdminPanel></AdminPanel>
+      </div>
+    )
+  }
+};
+
+export default InfluencerChart;
