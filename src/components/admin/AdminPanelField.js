c7d25f321c67fa90580c3159e7eccc45dd26dc6a
Antonio Colmenares <colmena.anto@gmail.com>
2019-09-13 18:24:53 +0200

    getting all the info filtered in the admin tab and if you click product you can update it

--- src/components/admin/AdminPanelField.js
+++ src/components/admin/AdminPanelField.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-
+import Router from 'next/router';
+import ProductServices from "../../services/ProductServices";
+import ReviewServices from "../../services/ReviewServices";
 
 class AdminPanelField extends Component {
   state = {
-    influencers: {}
+    influencers: {},
+    product: "",
+    review: ""
   };
 
+  getAll = (id) => {
+    ProductServices.getOneAdmin(id)
+      .then(product => this.setState({
+        product: product
+      }))
+      .catch((err) => console.log(err))
+
+    ReviewServices.getReviewAdmin(id)
+      .then(review => this.setState({
+        review: review
+      }))
+      .catch((err) => console.log(err))
+  }
+
+  componentDidMount = () => {
+    this.getAll(this.props.influencer._id);
+  }
+
+  onClickProduct = () => {
+    Router.push('/productUpdate/' + this.state.product[0]._id);
+  }
+
+  //{this.props.influencer._id}
+  //{this.state.product[0]._id}
+  //{this.state.review._id}
+
   render() {
-    console.log(this.props.influencers)
+    if (this.state.product && this.state.review){
     return(
       <ul>
-        <li>{this.props.influencer._id}</li>
-        <li>{this.props.influencer.name.firstName}</li>
-        <li>{this.props.influencer.name.lastName}</li>
+        <li>Influencer: {this.props.influencer.name.firstName} {this.props.influencer.name.lastName}</li>
+        <li onClick={this.onClickProduct}>Product: {this.state.product[0].model}</li>
+        <li>Review: {this.state.review.title}</li>
       </ul>
     )
+    } else {
+      return(
+        <ul>
+          <li>loading...</li>
+        </ul>
+      )
+    }
   }
 }
 
