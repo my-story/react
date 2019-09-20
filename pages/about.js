8e1dba43d6305fcfd67b6a05bc5d7b27ef7e804c
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-09-20 10:37:46 -0400

    about us

--- pages/about.js
+++ pages/about.js
@@ -12,25 +12,44 @@ function About(){
             	</div>
           </div>
           <div className="about-description">
-							<div className="description">
+				<div className="description">
                 <p>We do this by handpicking specific experts and asking them for a product, place, experience, etc., which has helped them personally be successful in their careers and achieve their goals. Luckily, they have decided to share their insights with you so that you can unlock your true potential</p>
-							</div>
+				</div>
         		<div className="video-section">
           			<div className="video-message">
             		<h1><b>What is it all about?</b></h1>
             		<p>Why wouldn't you trust the best of the best? <br></br></p>
             		<span><b>Watch a short video about us</b></span>
           		</div>
-							<div id="video-about-us-container">
-								<input type="image" id="video-start" src="https://res.cloudinary.com/dpt8pbi8n/image/upload/v1566406923/btn.svg" />
-								<img id="about-us-video" src="https://res.cloudinary.com/dpt8pbi8n/image/upload/v1566405071/Video.png" alt="video-about-us"/>
-							</div>
+					<div id="video-about-us-container">
+						<input type="image" id="video-start" src="https://res.cloudinary.com/dpt8pbi8n/image/upload/v1566406923/btn.svg" />
+						<img id="about-us-video" src="https://res.cloudinary.com/dpt8pbi8n/image/upload/v1566405071/Video.png" alt="video-about-us"/>
+					</div>
         		</div>
 					</div>
 					<div className="about-who-are-we">
-						<div><span><b>Who are we?</b></span></div>
-						<div></div>
-						<div></div>
+						<div id="who-title"><span><b>Who are we?</b></span></div>
+						<div id="who-description"><span>Few words about your team and corporate values. Lorem ipsum dolor. Few words about your team and corporate values. Lorem ipsum dolor.</span></div>
+						<div className="about-workers-container">
+							<div className="about-workers-card">
+								<div><img id="about-picture" alt="Sages" src="https://fsmedia.imgix.net/d9/6a/5a/bb/4e8f/478d/b2b2/7cb996c00019/heres-a-recap-of-elon-musks-crazy-2015.jpeg"/></div>
+								<div className="about-worker-name">
+									<h5>Gmoney Squad</h5>
+									<p>Co-founder</p>
+								</div>
+							</div>
+
+							<div className="about-workers-card">
+								<div><img id="about-picture" alt="Sages" src="https://fsmedia.imgix.net/d9/6a/5a/bb/4e8f/478d/b2b2/7cb996c00019/heres-a-recap-of-elon-musks-crazy-2015.jpeg"/></div>
+								<div className="about-worker-name">
+									<h5>Tony Boi</h5>
+									<p>Co-founder</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="about-donate-container">
+
 					</div>
         </div>
     )
