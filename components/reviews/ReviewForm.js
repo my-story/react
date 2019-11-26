be85bf2f475d1680905cc57acb576911b4dc3765
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-11-26 16:12:55 -0500

    survival kit

--- components/reviews/ReviewForm.js
+++ components/reviews/ReviewForm.js
@@ -1,8 +1,8 @@
-import React,{ Component } from 'react'
-// import { Redirect} from 'react-router-dom'
+import React,{ Component } from 'react';
+// import { Redirect} from 'react-router-dom';
 import Router from 'next/router';
 import { Input } from 'antd';
-import ReviewServices from "../../services/ReviewServices"
+import ReviewServices from "../../services/ReviewServices";
 
 const { TextArea } = Input;
 
